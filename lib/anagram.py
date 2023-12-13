e49de0253a402d089fc2c195e56bed31bdbac104
dmbeastz <daveroymutisya2@gmail.com>
2023-12-13 22:14:04 +0300

    Completed Lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,9 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, word_list):
+        anagram_match = [name for name in word_list if sorted(self.word) == sorted(name)]
+        return anagram_match
+
+
